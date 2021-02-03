518bd7f508e3ff50d34bba194ab914c7f9150765
zhaosongshun <243378654@qq.com>
2021-02-03 14:38:12 +0800

    初步配置

--- src/main.js
+++ src/main.js
@@ -1,16 +1,37 @@
 
-import './css/index.css';
-import './css/index.less';
 
-const add = function add() {
-  return '123';
-};
+import Vue from 'vue'
 
-console.log(add());
-const promise = new Promise((resolve) => {
-  setTimeout(() => {
-    console.log('aaaa');
-    resolve();
-  }, 1000);
-});
-console.log(promise);
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n 
+
+import moment from 'moment'
+
+
+import '@/styles/index.scss' // global css
+import App from './App'
+import router from './router/routers'
+import store from './store'
+
+import '@/icons' // icon
+import './router/index' // permission control 
+
+
+Vue.use(permission)
+Vue.use(ElementUI, {
+  // size: 'mini',
+  locale
+})
+moment.locale('zh-cn')
+Vue.use(moment)
+
+
+Vue.config.productionTip = false
+
+var app = new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+}) 
