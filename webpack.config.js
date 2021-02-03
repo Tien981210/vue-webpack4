b9df9f07546b72c04b6bead094f2498df891aabc
zhaosongshun <243378654@qq.com>
2021-02-03 11:52:10 +0800

    配置开发环境命令，以及打包去缓存

--- webpack.config.js
+++ webpack.config.js
@@ -11,74 +11,79 @@ const commomCssLoader = [
         loader: 'postcss-loader'
     }
 ]
+process.env.NODE_ENV = 'production'
 
 module.exports = {
     entry: './src/main.js',
     output: {
-        filename: 'static/main.js',
+        filename: 'static/main.[contenthash:10].js',
         path: resolve(__dirname, 'dist')
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_module/,
-                loader: [{
-                    loader: 'babel-loader',
+        rules: [{
+            oneOf: [
+
+                {
+                    test: /\.js$/,
+                    exclude: /node_module/,
+                    loader: [{
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [['@babel/preset-env', {
+                                useBuiltIns: 'usage',
+                                corejs: {
+                                    version: 3
+                                },
+                                targets: {
+                                    chrome: '60',
+                                    firefox: '60',
+                                    ie: '9',
+                                    safari: '10',
+                                    edge: '17'
+                                }
+                            }]],
+                            cacheDirectory: true
+                        }
+                    },
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    use: [...commomCssLoader
+                    ]
+                },
+                {
+                    test: /\.less$/,
+                    use: [
+                        ...commomCssLoader,
+                        'less-loader',
+                    ]
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    loader: 'url-loader',
                     options: {
-                        presets: [['@babel/preset-env', {
-                            useBuiltIns: 'usage',
-                            corejs: {
-                                version: 3
-                            },
-                            targets: {
-                                chrome: '60',
-                                firefox: '60',
-                                ie: '9',
-                                safari: '10',
-                                edge: '17'
-                            }
-                        }]],
-                        cacheDirectory: true
+                        limit: 8 * 1024,
+                        name: '[hash:10].[ext]',
+                        outputPath: 'imgs',
+                        esModule: false
                     }
                 },
-                ],
-            }, {
-                test: /\.css$/,
-                use: [...commomCssLoader
-                ]
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    ...commomCssLoader,
-                    'less-loader',
-                ]
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 8 * 1024,
-                    name: '[hash:10].[ext]',
-                    outputPath: 'imgs',
-                    esModule: false
-                }
-            },
-            {
-                test: /\.html$/,
-                loader: 'html-withimg-loader'
-            },
-            {
-                exclude: [/\.(css|less|jpg|png|gif|js|html)$/, /node_modules/],
-                loader: 'file-loader',
-                options: {
-                    // name: '[hash:10].[ext]',
-                    outputPath: 'file',
-                    esModule: false
-                }
-            },
-
+                {
+                    test: /\.html$/,
+                    loader: 'html-withimg-loader'
+                },
+                {
+                    exclude: [/\.(css|less|jpg|png|gif|js|html)$/, /node_modules/],
+                    loader: 'file-loader',
+                    options: {
+                        // name: '[hash:10].[ext]',
+                        outputPath: 'file',
+                        esModule: false
+                    }
+                },
+            ]
+        }
         ]
     },
     plugins: [
@@ -90,13 +95,13 @@ module.exports = {
                 // 折叠空格
                 collapseWhitespace: true,
                 // 移除注释
-                removeComments: true,
+                removeComments: true
             }
         }),
         new MiniCssExtractPlugin(
             {
-                filename: 'static/index.css',
-                chunkFilename: 'static/[id].css',
+                filename: 'static/index.[contenthash:10].css',
+                chunkFilename: 'static/[id].[contenthash:10].css',
             }),
         new OptimizeCssAssetsPlugin()
     ],
@@ -106,5 +111,14 @@ module.exports = {
         }
     },
     mode: 'production',//生产模式自动压缩js代码 
+    // mode: 'development',开发环境
+    devServer: {
+        contentBase: resolve(__dirname, 'build'),
+        compress: true,
+        port: 8373,
+        open: true,
+        // hot: true
+    },
+    devtool: 'cheap-source-map'
 
 }
